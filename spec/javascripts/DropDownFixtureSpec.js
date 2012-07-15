5fbfcb668b522393d4b1afe99137327d17396321
Bas van der Hoek <bas@hoek.com>
2012-07-15 22:16:36 +0200

    extracted fixture specs

---
+++ spec/javascripts/DropDownFixtureSpec.js
@@ -0,0 +1,15 @@
+describe("Drop-down fixture", function() {
+
+  beforeEach(function() {
+    loadFixtures('drop_down_fixture.html');
+  });
+	
+  it("should always load fixture", function() {
+    expect($('.drop_down')).toBeVisible();
+  });
+
+  it("should have only one drop-down", function() {
+    expect($('.drop_down').length).toBe(1);
+  });
+
+});
