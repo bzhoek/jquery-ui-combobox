c9c04b224faf1612bf7bf7676a3791cb79b93f28
Bas van der Hoek <bas@hoek.com>
2012-07-15 23:03:25 +0200

    keydown helper method

--- spec/javascripts/helpers/SpecHelper.js
+++ spec/javascripts/helpers/SpecHelper.js
@@ -1,3 +1,9 @@
+function keydown(name) {
+	var e = jQuery.Event("keydown");
+	e.keyCode = $.ui.keyCode[name];
+	return e;
+}
+
 beforeEach(function() {
   this.addMatchers({
     toBePlaying: function(expectedSong) {
