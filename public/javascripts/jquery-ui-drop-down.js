c94b281fde79502313b632556b42c3fd48ec7e42
Bas van der Hoek <bas@hoek.com>
2012-07-18 21:16:54 +0200

    show menu on arrow keys. ignore alphanumeric key presses. position menu below input. add mouse hover cursor and click handling

--- public/javascripts/jquery-ui-drop-down.js
+++ public/javascripts/jquery-ui-drop-down.js
@@ -16,7 +16,6 @@
 		},
 		
 		_create: function() {
-			console.log(this);
 			var self = this;
 			var options = self.options;
 			
@@ -25,13 +24,37 @@
 			
 			self.valueIndex = options.values.indexOf(options.selectedValue);
 			
-			self.list = $( "<ul>" ).insertAfter(self.element);
+			var position = $(self.element).position();
+			self.list = $( "<ul>" )
+				.addClass('ui-menu ui-widget ui-widget-content ui-corner-all')
+				.css('width', $(self.element).outerWidth())
+				.css('position', 'absolute')
+				.css('top', position.x)
+				.insertAfter(self.element);
 			self.list.addClass('jq_drop_down').hide();
 
 			$(options.values).each(function(i,value) {
-				$('<li>').text(value).appendTo(self.list);
+				var item = $('<li>');
+				var anchor = $('<a>').addClass('ui-corner-all').text(value);
+				item.addClass('ui-menu-item')
+						.append(anchor)
+						.appendTo(self.list);
+				item.on('mouseover', function(e) {
+					anchor.addClass('ui-state-hover')
+				});
+				item.on('mouseout', function(e) {
+					anchor.removeClass('ui-state-hover');
+				});
+				anchor.on('mousedown', function() {
+					var index = options.values.indexOf($(this).text());
+					self._setValueFromIndex(index);
+					setTimeout(function() {self.element.focus(); self.list.hide()}, 100);
+				});
 			});
 			
+			$(self.element).on('click', function() {
+				self.list.show();
+			});
 			$(self.element).on('focus', function() {
 				self.list.show();
 			});
@@ -39,21 +62,61 @@
 				self.list.hide();
 			});
 			
-			$(self.element).on('keydown', $.proxy(this._keyDown, this));
+			$(self.element).on('keydown', $.proxy(this._inputKeyDown, this));
+			$(self.element).on('keypress', $.proxy(this._inputKeyPress, this));
 		},
 
 		_setOption: function(key, value) {
+			console.log("clear");
 			switch (key) {
 			case "clear":
-					console.log("clear");
 			    break;
 			case "values":
-				console.log("values");
 				this.options.values = value;
 				break;
 			}
 
 			$.Widget.prototype._setOption.apply(this, arguments);
+		},
+
+		_itemClick: function(e) {
+			console.log(e);
+		},
+		
+		_inputKeyPress: function(e) {
+			var chr = String.fromCharCode(e.charCode);
+			var control = e.altKey || e.metaKey || e.ctrlKey;
+			if(!control && chr.match(/^[0-9a-zA-Z]+$/)) {
+				e.preventDefault();
+			}
+		},
+		
+		_inputKeyDown: function(e) {
+			var self = this;
+			var keyCode = $.ui.keyCode;
+			var values = this.options.values;
+			
+			switch(e.which) {
+        case keyCode.TAB:
+					break;
+        case keyCode.ESCAPE:
+          $(self.element).val(self.options.originalValue);
+          self.valueIndex = self.options.values.indexOf(self.options.originalValue);
+					self.list.hide();
+          break;
+				case keyCode.UP:
+					if(self.valueIndex > -1) {
+            self._setValueFromIndex(self.valueIndex-1);
+          }
+					self.list.show();
+					break;
+				case keyCode.DOWN:
+					if(self.valueIndex < values.length-1) {
+            self._setValueFromIndex(self.valueIndex+1);
+					}
+					self.list.show();
+					break;
+			}
 		},
 
     _setValueFromIndex: function(i) {
@@ -62,37 +125,12 @@
       this.valueIndex = i;
       $(this.element).val(i == -1 ? "" : this.options.values[i]);
 
-			items.removeClass('selected');
+			items.removeClass('ui-state-focus');
 			if(i != -1) {
-				$(items.get(i)).addClass('selected')
+				$(items.get(i)).addClass('ui-state-focus')
 			}
     },
-
-		_keyDown: function(e) {
-			var self = this;
-			var keyCode = $.ui.keyCode;
-			var values = this.options.values;
-			switch(e.which) {
-        case keyCode.ESCAPE:
-          $(self.element).val(self.options.originalValue);
-          self.valueIndex = self.options.values.indexOf(self.options.originalValue);
-          break;
-				case keyCode.UP:
-					if(self.valueIndex > -1) {
-            self._setValueFromIndex(self.valueIndex-1);
-          }
-					break;
-				case keyCode.DOWN:
-					if(self.valueIndex < values.length-1) {
-            self._setValueFromIndex(self.valueIndex+1);
-					}
-					break;
-			}
-			// console.log(this.options.values);
-			// console.log(this.list);
-			// console.log(this.element);
-		},
-
+		
 		// Use the destroy method to clean up any modifications your widget has made to the DOM
 		destroy: function() {
 			$.Widget.prototype.destroy.call(this);
